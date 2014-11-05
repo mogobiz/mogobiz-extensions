aa45b9e91bf6b29c6346d6b526fd895e91f88944
Stéphane Manciot <stephane.manciot@ebiznext.com>
2014-11-05 12:59:31 +0100

    update artefacts version to 0.1-SNAPSHOT

--- MogobizExtensionsGrailsPlugin.groovy
+++ MogobizExtensionsGrailsPlugin.groovy
@@ -1,7 +1,7 @@
 class MogobizExtensionsGrailsPlugin {
     def packaging = "binary"
     // the plugin version
-    def version = "1.0-SNAPSHOT"
+    def version = "0.1-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.3 > *"
     // resources that are excluded from plugin packaging
--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -44,9 +44,9 @@ grails.project.dependency.resolution = {
             excludes 'org.elasticsearch:elasticsearch'
         }
 
-        compile 'com.mogobiz.rivers:mogobiz-elasticsearch:1.0-SNAPSHOT'
-        compile 'com.mogobiz.rivers:mogobiz-google-shopping:1.0-SNAPSHOT'
-        compile 'com.mogobiz.rivers:mogobiz-cfp:1.0-SNAPSHOT'
+        compile 'com.mogobiz.rivers:mogobiz-elasticsearch:0.1-SNAPSHOT'
+        compile 'com.mogobiz.rivers:mogobiz-google-shopping:0.1-SNAPSHOT'
+        compile 'com.mogobiz.rivers:mogobiz-cfp:0.1-SNAPSHOT'
 
     }
 
@@ -55,7 +55,7 @@ grails.project.dependency.resolution = {
               ":rest-client-builder:1.0.3") {
             export = false
         }
-        compile group:"com.mogobiz", name:"mogobiz-core", version:"1.0-SNAPSHOT"
+        compile group:"com.mogobiz", name:"mogobiz-core", version:"0.1-SNAPSHOT"
 
     }
 }
