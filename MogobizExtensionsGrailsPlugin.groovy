dd313c001eb68695f41d9385c1477c87e43d5c5f
Stéphane Manciot <stephane.manciot@ebiznext.com>
2015-07-18 09:51:27 +0300

    update to version 0.2-SNAPSHOT

--- MogobizExtensionsGrailsPlugin.groovy
+++ MogobizExtensionsGrailsPlugin.groovy
@@ -1,7 +1,7 @@
 class MogobizExtensionsGrailsPlugin {
     def packaging = "binary"
     // the plugin version
-    def version = "0.1-SNAPSHOT"
+    def version = "0.2-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.3 > *"
     // resources that are excluded from plugin packaging
