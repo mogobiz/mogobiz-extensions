8f6a5d3d4f74cde630a29d4f3f0a865065330ca1
Hayssam Saleh <hayssam.saleh@ebiznext.com>
2015-10-11 20:24:12 +0200

    Add copyright notice

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,3 +1,7 @@
+/*
+ * Copyright (C) 2015 Mogobiz SARL. All rights reserved.
+ */
+
 class UrlMappings {
 
 	static mappings = {
--- src/groovy/com/mogobiz/store/Pagination.groovy
+++ src/groovy/com/mogobiz/store/Pagination.groovy
@@ -1,3 +1,7 @@
+/*
+ * Copyright (C) 2015 Mogobiz SARL. All rights reserved.
+ */
+
 package com.mogobiz.store
 
 import com.mogobiz.constant.IperConstant
