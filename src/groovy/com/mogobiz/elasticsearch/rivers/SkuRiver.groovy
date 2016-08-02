8668fefbd9fceb8582e11ceefdd65d8467887a6a
Stephane Manciot <stephane.manciot@ebiznext.com>
2016-08-02 16:18:56 +0300

    externalise common args

--- src/groovy/com/mogobiz/elasticsearch/rivers/SkuRiver.groovy
+++ src/groovy/com/mogobiz/elasticsearch/rivers/SkuRiver.groovy
@@ -36,36 +36,37 @@ class SkuRiver  extends AbstractESRiver<TicketType>{
         def defaultLang = config?.defaultLang ?: 'fr'
         def _defaultLang = defaultLang.trim().toLowerCase()
         def _languages = languages.collect {it.trim().toLowerCase()} - _defaultLang
+        final args = [readOnly: true, flushMode: FlushMode.MANUAL]
         if(!_languages.flatten().isEmpty()){
             Set<Translation> translations = []
             translations << Translation.executeQuery('select t from Product p, Translation t where t.target=p.id and t.lang in :languages and (p.category.catalog.id in (:idCatalogs) and p.state=:productState)',
-                    [languages:_languages, idCatalogs:config.idCatalogs, productState:ProductState.ACTIVE], [readOnly: true, flushMode: FlushMode.MANUAL])
+                    [languages:_languages, idCatalogs:config.idCatalogs, productState:ProductState.ACTIVE], args)
             translations << Translation.executeQuery('select t from Product p left join p.features as f, Translation t where t.target=f.id and t.lang in :languages and (p.category.catalog.id in (:idCatalogs) and p.state=:productState)',
-                    [languages:_languages, idCatalogs:config.idCatalogs, productState:ProductState.ACTIVE], [readOnly: true, flushMode: FlushMode.MANUAL])
+                    [languages:_languages, idCatalogs:config.idCatalogs, productState:ProductState.ACTIVE], args)
             translations << Translation.executeQuery('select t from Product p left join p.featureValues as fv, Translation t where t.target=fv.id and t.lang in :languages and (p.category.catalog.id in (:idCatalogs) and p.state=:productState)',
-                    [languages:_languages, idCatalogs:config.idCatalogs, productState:ProductState.ACTIVE], [readOnly: true, flushMode: FlushMode.MANUAL])
+                    [languages:_languages, idCatalogs:config.idCatalogs, productState:ProductState.ACTIVE], args)
             translations << Translation.executeQuery('select t from TicketType sku, Translation t where t.target=sku.id and t.lang in :languages and (sku.product.category.catalog.id in (:idCatalogs) and sku.product.state=:productState and (sku.stopDate is null or sku.stopDate >= :today))',
-                    [languages:_languages, idCatalogs:config.idCatalogs, productState:ProductState.ACTIVE, today: now], [readOnly: true, flushMode: FlushMode.MANUAL])
+                    [languages:_languages, idCatalogs:config.idCatalogs, productState:ProductState.ACTIVE, today: now], args)
             translations << Translation.executeQuery('select t from TicketType sku left join sku.variation1 as v1 left outer join sku.variation2 as v2 left outer join sku.variation3 as v3, Translation t where (t.target=v1.id or t.target=v1.variation.id or (v2 != null and (t.target=v2.id or t.target=v2.variation.id)) or (v3 != null and t.target=v3.id or t.target=v3.variation.id)) and t.lang in :languages and (sku.product.category.catalog.id in (:idCatalogs) and sku.product.state=:productState) and (sku.stopDate is null or sku.stopDate >= :today)',
-                    [languages:_languages, idCatalogs:config.idCatalogs, productState:ProductState.ACTIVE, today: now], [readOnly: true, flushMode: FlushMode.MANUAL])
+                    [languages:_languages, idCatalogs:config.idCatalogs, productState:ProductState.ACTIVE, today: now], args)
             translations << Translation.executeQuery('select t from Category cat, Translation t where t.target=cat.id and t.lang in :languages and cat.catalog.id in (:idCatalogs)',
-                    [languages:_languages, idCatalogs:config.idCatalogs], [readOnly: true, flushMode: FlushMode.MANUAL])
+                    [languages:_languages, idCatalogs:config.idCatalogs], args)
             translations << Translation.executeQuery('select t from Category cat left join cat.features as f, Translation t where t.target=f.id and t.lang in :languages and cat.catalog.id in (:idCatalogs)',
-                    [languages:_languages, idCatalogs:config.idCatalogs], [readOnly: true, flushMode: FlushMode.MANUAL])
+                    [languages:_languages, idCatalogs:config.idCatalogs], args)
             translations << Translation.executeQuery('select t from Brand brand, Translation t where t.target=brand.id and t.lang in :languages and brand.company in (select c.company from Catalog c where c.id in (:idCatalogs))',
-                    [languages:_languages, idCatalogs:config.idCatalogs], [readOnly: true, flushMode: FlushMode.MANUAL])
+                    [languages:_languages, idCatalogs:config.idCatalogs], args)
             translations << Translation.executeQuery('select t from Tag tag, Translation t where t.target=tag.id and t.lang in :languages and tag.company in (select c.company from Catalog c where c.id in (:idCatalogs))',
-                    [languages:_languages, idCatalogs:config.idCatalogs], [readOnly: true, flushMode: FlushMode.MANUAL])
+                    [languages:_languages, idCatalogs:config.idCatalogs], args)
             translations << Translation.executeQuery('select t from Product2Resource pr left join pr.product as p left join pr.resource as r, Translation t where t.target=r.id and t.lang in :languages and (p.category.catalog.id in (:idCatalogs) and p.state=:productState)',
-                    [languages:_languages, idCatalogs:config.idCatalogs, productState:ProductState.ACTIVE], [readOnly: true, flushMode: FlushMode.MANUAL])
+                    [languages:_languages, idCatalogs:config.idCatalogs, productState:ProductState.ACTIVE], args)
             translations << Translation.executeQuery('select t from Coupon coupon join coupon.products as p, Translation t where t.target=coupon.id and t.lang in :languages and (p.category.catalog.id in (:idCatalogs) and p.state=:productState)',
-                    [languages:_languages, idCatalogs:config.idCatalogs, productState:ProductState.ACTIVE], [readOnly: true, flushMode: FlushMode.MANUAL])
+                    [languages:_languages, idCatalogs:config.idCatalogs, productState:ProductState.ACTIVE], args)
             translations << Translation.executeQuery('select t from Coupon coupon join coupon.categories as category, Translation t where t.target=coupon.id and t.lang in :languages and (category.catalog.id in (:idCatalogs) and coupon.active=true)',
-                    [languages:_languages, idCatalogs:config.idCatalogs], [readOnly: true, flushMode: FlushMode.MANUAL])
+                    [languages:_languages, idCatalogs:config.idCatalogs], args)
             translations << Translation.executeQuery('select t from Coupon coupon join coupon.ticketTypes as ticketType, Translation t where t.target=coupon.id and t.lang in :languages and (ticketType.product.category.catalog.id in (:idCatalogs) and ticketType.product.state=:productState and (ticketType.stopDate is null or ticketType.stopDate >= :today) and coupon.active=true)',
-                    [languages:_languages, idCatalogs:config.idCatalogs, productState:ProductState.ACTIVE, today: now], [readOnly: true, flushMode: FlushMode.MANUAL])
+                    [languages:_languages, idCatalogs:config.idCatalogs, productState:ProductState.ACTIVE, today: now], args)
             translations << Translation.executeQuery('select t from Coupon coupon join coupon.catalogs as catalog, Translation t where t.target=coupon.id and t.lang in :languages and (catalog.id in (:idCatalogs) and coupon.active=true)',
-                    [languages:_languages, idCatalogs:config.idCatalogs], [readOnly: true, flushMode: FlushMode.MANUAL])
+                    [languages:_languages, idCatalogs:config.idCatalogs], args)
             translations.flatten().groupBy {"${it.target}"}.each {k, v ->
                 TranslationsRiverCache.instance.put(k, v)
             }
@@ -75,7 +76,7 @@ class SkuRiver  extends AbstractESRiver<TicketType>{
         def couponsMap = [:]
 
         Coupon.executeQuery('select product, coupon FROM Coupon coupon left join fetch coupon.rules left join coupon.products as product where (product.category.catalog.id in (:idCatalogs) and product.state=:productState and coupon.active=true)',
-                [idCatalogs:config.idCatalogs, productState:ProductState.ACTIVE], [readOnly: true, flushMode: FlushMode.MANUAL]).each {a ->
+                [idCatalogs:config.idCatalogs, productState:ProductState.ACTIVE], args).each { a ->
             def key = (a[0] as Product).uuid
             Set<Coupon> coupons = couponsMap.get(key) as Set<Coupon> ?: []
             coupons.add(a[1] as Coupon)
@@ -83,7 +84,7 @@ class SkuRiver  extends AbstractESRiver<TicketType>{
         }
 
         Coupon.executeQuery('select ticketType, coupon FROM Coupon coupon left join fetch coupon.rules left join coupon.ticketTypes as ticketType left join ticketType.product as product where (product.category.catalog.id in (:idCatalogs) and product.state=:productState and (ticketType.stopDate is null or ticketType.stopDate >= :today) and coupon.active=true)',
-                [idCatalogs:config.idCatalogs, productState:ProductState.ACTIVE, today: now], [readOnly: true, flushMode: FlushMode.MANUAL]).each {a ->
+                [idCatalogs:config.idCatalogs, productState:ProductState.ACTIVE, today: now], args).each { a ->
             def key = (a[0] as TicketType).uuid
             Set<Coupon> coupons = couponsMap.get(key) as Set<Coupon> ?: []
             coupons.add(a[1] as Coupon)
@@ -91,7 +92,7 @@ class SkuRiver  extends AbstractESRiver<TicketType>{
         }
 
         Coupon.executeQuery('select category, coupon FROM Coupon coupon left join fetch coupon.rules left join coupon.categories as category where category.catalog.id in (:idCatalogs) and coupon.active=true',
-                [idCatalogs:config.idCatalogs], [readOnly: true, flushMode: FlushMode.MANUAL]).each {a ->
+                [idCatalogs:config.idCatalogs], args).each { a ->
             def key = (a[0] as Category).uuid
             Set<Coupon> coupons = couponsMap.get(key) as Set<Coupon> ?: []
             coupons.add(a[1] as Coupon)
@@ -99,7 +100,7 @@ class SkuRiver  extends AbstractESRiver<TicketType>{
         }
 
         Coupon.executeQuery('select catalog, coupon FROM Coupon coupon left join fetch coupon.rules left join coupon.catalogs as catalog where catalog.id in (:idCatalogs) and coupon.active=true',
-                [idCatalogs:config.idCatalogs], [readOnly: true, flushMode: FlushMode.MANUAL]).each {a ->
+                [idCatalogs:config.idCatalogs], args).each { a ->
             def key = (a[0] as Catalog).uuid
             Set<Coupon> coupons = couponsMap.get(key) as Set<Coupon> ?: []
             coupons.add(a[1] as Coupon)
@@ -133,7 +134,7 @@ class SkuRiver  extends AbstractESRiver<TicketType>{
                         'left join fetch p.taxRate as taxRate ' +
                         'left join fetch taxRate.localTaxRates ' +
                         'WHERE p.category.catalog.id in (:idCatalogs) and p.state = :productState and p.deleted = false and (sku.stopDate is null or sku.stopDate >= :today)',
-                [idCatalogs:config.idCatalogs, productState:ProductState.ACTIVE, today: now], [readOnly: true, flushMode: FlushMode.MANUAL])
+                [idCatalogs:config.idCatalogs, productState:ProductState.ACTIVE, today: now], args)
         )
     }
 
